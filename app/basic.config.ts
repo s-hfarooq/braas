3eab495702d425e5047c1b8093573d9f881ecfab
Timothy Vitkin <timothy.vitkin@gmail.com>
2025-02-22 12:14:25 -0800

    basic: update schema

--- app/basic.config.ts
+++ app/basic.config.ts
@@ -5,17 +5,44 @@
 // Project: braas
 // Link: https://app.basic.tech/project/a9cf1e85-1cb1-461e-9c04-fcb5e0908369
 
+
 export const schema = {
   "project_id": "a9cf1e85-1cb1-461e-9c04-fcb5e0908369",
   "tables": {
-    "emojis": {
+    "video": {
+      "type": "collection",
       "fields": {
-        "value": {
-          "type": "string"
+        "prompt": {
+          "type": "string",
+          "indexed": true
+        },
+        "description": {
+          "type": "string",
+          "indexed": true
+        },
+        "content": {
+          "type": "string",
+          "indexed": true
+        },
+        "metadata": {
+          "type": "json",
+          "indexed": true
         }
-      },
-      "type": "collection"
+      }
+    },
+    "prompts": {
+      "type": "collection",
+      "fields": {
+        "topic": {
+          "type": "string",
+          "indexed": true
+        },
+        "output": {
+          "type": "string",
+          "indexed": true
+        }
+      }
     }
   },
-  "version": 0
-};
+  "version": 1
+}
